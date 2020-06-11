e658a020139b422fb4a38fcf5e972c18e7678519
zieed <42714947+zieed@users.noreply.github.com>
2020-06-11 18:08:04 +0200

    initial

---
+++ ServiceStage/ServiceStage/src/main/java/org/iset/sbz/ServiceStage/ServiceStageApplication.java
@@ -0,0 +1,13 @@
+package org.iset.sbz.ServiceStage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceStageApplication.class, args);
+	}
+
+}
---
+++ ServiceStage/ServiceStage/src/test/java/org/iset/sbz/ServiceStage/ServiceStageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iset.sbz.ServiceStage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceStageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
